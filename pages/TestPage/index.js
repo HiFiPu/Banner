f63338d80756a815d2ffa294a6472a5d82758225
HiFiPu <hifipugongying@163.com>
2022-02-13 22:08:21 +0800

    'newFiles'

---
+++ pages/TestPage/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import "./style.less"
+export default class index extends Component {
+  render() {
+    return (
+      <ul>
+          <li>
+              <h1>利用 Bootstrap 构建快速、响应式的网站</h1>
+              <p>Bootstrap 是全球最受欢迎的前端开源工具库，它支持 Sass 变量和 mixin、响应式栅格系统、自带大量组件和众多强大的 JavaScript 插件。基于 Bootstrap 提供的强大功能，能够让你快速设计并定制你的网站。</p>
+          </li>
+          <li>
+          <h1>利用 Bootstrap 构建快速、响应式的网站</h1>
+              <p>Bootstrap 是全球最受欢迎的前端开源工具库，它支持 Sass 变量和 mixin、响应式栅格系统、自带大量组件和众多强大的 JavaScript 插件。基于 Bootstrap 提供的强大功能，能够让你快速设计并定制你的网站。</p>
+          </li>
+          <li>
+          <h1>利用 Bootstrap 构建快速、响应式的网站</h1>
+              <p>Bootstrap 是全球最受欢迎的前端开源工具库，它支持 Sass 变量和 mixin、响应式栅格系统、自带大量组件和众多强大的 JavaScript 插件。基于 Bootstrap 提供的强大功能，能够让你快速设计并定制你的网站。</p>
+          </li>
+      </ul>
+    )
+  }
+}
